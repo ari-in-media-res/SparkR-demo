6b79cdb5ee4a7ce7c123cbf689302d5998ef49ac
Vedant Jain <vedjain85@gmail.com>
2015-06-25 12:48:58 -0500

    Create wordcount.R

---
+++ wordcount.R
@@ -0,0 +1,19 @@
+library(SparkR)
+
+
+# Initialize Spark context
+
+lines <- textFile(sc, "/demo/data/shakespeare.txt")
+
+words <- flatMap(lines,
+                 function(line) {
+                   strsplit(line, " ")[[1]]
+                 })
+wordCount <- lapply(words, function(word) { list(word, 1L) })
+
+counts <- reduceByKey(wordCount, "+", 2L)
+output <- collect(counts)
+
+for (wordcount in output) {
+  cat(wordcount[[1]], ": ", wordcount[[2]], "\n")
+}
